58609eedc314defd5115441d01dc6e715c587921
kurt spencer <KurtSpencer@kurts-MacBook-Pro.local>
2017-01-05 10:39:26 -0700

    added sidenav

--- public/js/module.home.js
+++ public/js/module.home.js
@@ -1,12 +1,13 @@
-// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
 }
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
 /************************* Home Page Controller ************************************/
 angular.module('module.home', [])
     .controller('performanceController', perfControl)
